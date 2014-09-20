2a07ac98bbba39f0704c5a3143de69cac1512114
Michael Terry <michael@michaelterry.org>
2014-09-20 05:56:00 +0000

    start fleshing out tinker lib

--- tinker.js
+++ tinker.js
@@ -1,7 +1,35 @@
-// tinker.synchLoadScriptsFromUrls('http://cdnjs.cloudflare.com/ajax/libs/js-signals/1.0.0/js-signals.min.js',
-// 		   'http://cdnjs.cloudflare.com/ajax/libs/crossroads/0.12.0/crossroads.js');
+function Tinker() {
+  phantom.onError = function(msg, trace) {
+    var msgStack = ['PHANTOM ERROR: ' + msg];
+    if (trace) {
+      msgStack.push('TRACE:');
+      trace.forEach(function(t) {
+        msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+      });
+    }
+    console.error(msgStack.join('\n'));
+    phantom.exit(1);
+  };
 
-function Tinker() {};
+  phantom.injectJs('js-signals.min.js');
+  phantom.injectJs('crossroads');
+
+  var webPage = require('webpage');
+  this.page = webPage.create();
+
+  this.page.onConsoleMessage = function(msg, lineNum, sourceId) {
+    console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+  };
+
+  this.page.onLoadFinished = function(status) {
+    console.log('Status: ' + status);
+    console.log('Url: ' + this.page.url);
+    crossroads.parse(this.page.url);
+
+    // Do other things here...
+  };
+
+};
 
 function testLoadScripts() {
     console.log("Starting testLoadScripts...");

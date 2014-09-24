f36599f7278b28a195ff087f0b429e42967f39b2
Michael Terry <michael@michaelterry.org>
2014-09-24 21:41:32 +0000

    get class data as json

--- school1.js
+++ school1.js
@@ -1,4 +1,4 @@
-// PHJS_USER=username PHJS_PASS=password phantomjs --web-security=false school1.js
+// PORTAL_PIN=username PORTAL_PASS=password phantomjs --web-security=false school1.js
 
 phantom.injectJs('tinker.js');
 tinker.synchLoadScriptsFromUrls('http://localhost:8080/skewer');
@@ -20,17 +20,54 @@ phantom.onError = function(msg, trace) {
     phantom.exit(1);
 };
 
-crossroads.addRoute('https://qweb.venturausd.org/parentportal/', function() {
-  var system = require('system');
-  var user = system.env.PHJS_USER;
-  var pass = system.env.PHJS_PASS;
-
-  page.evaluate(function(credentials) {
-    jQuery('input#Pin').val(credentials[0]);
-    jQuery('input#Password').val(credentials[1]);
+function login(keys) {
+    jQuery('input#Pin').val(keys[0]);
+    jQuery('input#Password').val(keys[1]);
     jQuery('input#LoginButton').click();
     console.log("finished login...");
-  }, [user, pass]);
+}
+
+function selectStudent(Id) {
+    jQuery('#' + Id).click();
+}
+
+function assignments(tbl) {
+  var columns = jQuery(tbl).find('thead th').map(function() {
+    return $(this).text();
+  });
+
+  return jQuery(tbl).find('tbody tr').map(function(i) {
+    var row = {};
+    $(this).find('td').each(function(i) {
+      var rowName = columns[i];
+      row[rowName] = $(this).text();
+    });
+    return row;
+  }).get();
+}
+
+function course(tbl) {
+  var course = {};
+  course['name'] = jQuery(tbl).find('tr:nth-child(1)')
+    .find('b:nth-child(2)').text().trim().split(" ")[0];
+  course['grade'] = jQuery(tbl).find('tr:nth-child(2) td:nth-child(1)')
+    .first().contents().eq(2).text().split('\n')[0];
+  course['assignments'] = assignments(tbl);
+  return course;
+}
+
+function report() {
+  return jQuery('table[id^=tblassign').map(function() {
+    return course(this);
+  }).get();
+}
+
+crossroads.addRoute('https://qweb.venturausd.org/parentportal/', function() {
+  var system = require('system');
+  var user = system.env.PORTAL_PIN;
+  var pass = system.env.PORTAL_PASS;
+
+  page.evaluate(login, [user, pass]);
 });
 
 var webPage = require('webpage');
